5b92010c8b0f4264058cd1489f2e100abdd10775
kalyani joshi <kalyani121@gmail.com>
2021-10-10 21:18:07 -0400

    updated mutations to save and delete books

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,4 +1,9 @@
 const { Book, User } = require('../models');
+const jwt = require('jsonwebtoken');
+const { signToken } = require('../utils/auth');
+
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
 
 const resolvers = {
   Query: {
@@ -14,8 +19,61 @@ const resolvers = {
   Mutation: {
     createUser: async (parent, args) => {
       const user = await User.create(args);
-      console.log(user)
-      return user;
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { username, email, password }) => {
+      const user = await User.findOne({
+        $or: [{ username: username }, { email: email }],
+      });
+      if (!user) {
+        return { message: "Can't find this user" };
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        return { message: 'Wrong password!' };
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, args) => {
+      console.log(args);
+      const { data } = jwt.verify(args.token, secret, { maxAge: expiration });
+      console.log(data);
+      const book = {
+        bookId: args.bookId,
+        title: args.title,
+        authors: args.authors,
+        description: args.description,
+        image: args.image,
+        link: args.link,
+      };
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: data._id },
+        { $addToSet: { savedBooks: book } },
+        { new: true, runValidators: true }
+      );
+      return updatedUser;
+    },
+    getSingleUser: async (parent, args) => {
+      const { data } = jwt.verify(args.token, secret, { maxAge: expiration });
+      const foundUser = await User.findOne({
+        $or: [{ _id: user ? data._id : args.id }, { username: args.username }],
+      });
+
+      return foundUser;
+    },
+    deleteBook: async (parent, args) => {
+      const { data } = jwt.verify(args.token, secret, { maxAge: expiration });
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: data._id },
+        { $pull: { savedBooks: { bookId: args.bookId } } },
+        { new: true }
+      );
+
+      return updatedUser;
     },
   },
 };
--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -9,8 +9,15 @@ const typeDefs = gql`
     savedBooks: [Book]
   }
 
+  type Auth {
+    user: User!
+    token: String!
+    message: String
+  }
+
   type Book {
     _id: ID!
+    bookId: String!
     title: String!
     authors: [String!]
     description: String!
@@ -19,12 +26,18 @@ const typeDefs = gql`
   }
 
   type Query {
-    user(_id: ID): [User]
+    user(_id: ID, username: String): [User]
     book(_id: ID): [Book]
   }
 
   type Mutation {
-    createUser(username: String!, email: String!, password: String!): User
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(username: String, email: String, password: String!): Auth
+    saveBook(token: String!, bookId: String!, title: String!, authors: [String!], description: String!,
+      image: String!,
+      link: String!): User
+    getSingleUser(token: String, id: ID, username: String): User
+    deleteBook(token: String!, bookId: String!): User
   }
 `;
 
